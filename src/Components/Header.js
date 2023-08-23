9dd26b8fee3325d35bebc4d9e0d8135654486e83
yash-zer0 <108175658+yash-zer0@users.noreply.github.com>
2023-08-23 12:49:02 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ handleToggleDarkMode }) => {
 	return (
 		<div className='header'>
 			<h1>NoteTaker</h1>
-			<p>ONE PLACE FOR ALL YOUR NOTES</p>
+			<p style={{color:'aqua'}}>ONE PLACE FOR ALL YOUR NOTES</p>
 			<button
 				onClick={() =>
 					handleToggleDarkMode(
